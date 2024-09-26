a1ac6d336b440f74f174ac921f62041284c60367
hkhangus <huukhangtc@gmail.com>
2024-09-26 14:31:47 +0700

    fix: remove telegram.webapp

--- src/components/Invite/Invite.tsx
+++ src/components/Invite/Invite.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-<script src="https://telegram.org/js/telegram-web-app.js"></script>
 
 declare global {
   interface Window {
@@ -12,21 +11,10 @@ export const Invite = () => {
     console.log("Invite link copied");
     navigator.clipboard.writeText(`https://t.me/botvjp1_bot/join?startapp=${123}`);
   };
-  const tg = window.Telegram.WebApp;
-
-// Parse the initialization data (initData) which contains 'start_param'
-const initData = tg.initData;
-const initDataUnsafe = tg.initDataUnsafe;
-
-// 'start_param' will be inside 'initDataUnsafe.query_id'
-console.log("Telegram Init Data:", initData);
-console.log("Telegram Init Data Unsafe:", initDataUnsafe);
-
-// Extract 'start_param' if available
-const startParam = initDataUnsafe.start_param || null;
-
-console.log("Start Param:", startParam);
-
+  
+  const urlParams = new URLSearchParams(window.location.search);
+  const startParam = urlParams.get('startapp');
+  
   return (
     <>
       <h1>Invite</h1>
