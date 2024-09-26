b446781356fe6aa2d740d7e97a47e9ab4c8331a3
hkhangus <huukhangtc@gmail.com>
2024-09-26 14:03:47 +0700

    chore: update get start_param

--- src/components/Invite/Invite.tsx
+++ src/components/Invite/Invite.tsx
@@ -7,31 +7,13 @@ declare global {
 }
 
 export const Invite = () => {
-  const [startParam, setStartParam] = useState(null);
-
   const copyInviteLink = () => {
     console.log("Invite link copied");
     navigator.clipboard.writeText(`https://t.me/botvjp1_bot/join?startapp=${123}`);
   };
   
-  useEffect(() => {
-    // Ensure that the Telegram WebApp SDK is loaded
-    const loadTelegramSDK = () => {
-      if (window.Telegram) {
-        // Mark the Web App as ready
-        window.Telegram.WebApp.ready();
-
-        // Get the initData and start parameter
-        const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
-        const startParamValue = initDataUnsafe.query_id || initDataUnsafe.start_param;
-
-        // Set the start parameter in the component state
-        setStartParam(startParamValue);
-      }
-    };
-
-    loadTelegramSDK();
-  }, []); // Run once on mount
+  const urlParams = new URLSearchParams(window.location.search);
+  const startParam = urlParams.get('startapp');
   return (
     <>
       <h1>Invite</h1>
