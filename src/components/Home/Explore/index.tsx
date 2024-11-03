1ec9451fb767707c1be0f0340ab79f4b32d14184
hkhangus <huukhangtc@gmail.com>
2024-11-04 00:07:55 +0700

    chore: update UI explore

--- src/components/Home/Explore/index.tsx
+++ src/components/Home/Explore/index.tsx
@@ -1,38 +1,21 @@
-import { Box, Button, Card, Flex, Heading } from '@radix-ui/themes';
+import { Box } from '@radix-ui/themes';
 
-// import PageContainer from 'components/Common/Page/PageContainer';
 import AppPageContainer from 'components/Common/Page/AppPageContainer';
 import { useAppContext } from 'context/app';
 
+import Header from './Header';
 import Quest from './Quest';
+import User from './User';
 
 export default function Explore() {
   const { onUIChange } = useAppContext();
 
   return (
     <AppPageContainer>
-      <Heading
-        as="h2"
-        size="6"
-        color="amber"
-      >
-        Pirate Treasure
-      </Heading>
-      <Button onClick={() => onUIChange('home')}>Back</Button>
+      <Header />
       <Box className="text-left">
         <Box className="flex justify-center mt-10">
-          <Card className=" flex space-x-4">
-            <Box className="flex flex-col">
-              <Heading
-                as="h2"
-                size="3"
-              >
-                Bob
-              </Heading>
-              <span className=" text-1">ID: 123456789</span>
-            </Box>
-            <Button>Copy</Button>
-          </Card>
+          <User />
         </Box>
       </Box>
 
