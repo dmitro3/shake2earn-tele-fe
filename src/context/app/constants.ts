399e244f4357e740efe048429a70b0326c9f1fde
nngthinh <dnngthinh@gmail.com>
2024-11-04 08:57:04 +0700

    fix: public base url for assets

--- src/context/app/constants.ts
+++ src/context/app/constants.ts
@@ -1,25 +1,29 @@
 import { AssetConfig, AssetType } from 'hooks/common/useLoadAssets';
 
+const withViteBaseUrlWrapper = (url: string) => {
+  return import.meta.env.BASE_URL ? url : `${import.meta.env.BASE_URL}/${url}`;
+}
+
 // Assets from public folder will be cached
 export const AppAssets: AssetConfig[] = [
   {
     type: AssetType.IMG,
     name: 'background',
-    src: 'assets/app/ocean-background.png',
+    src: withViteBaseUrlWrapper('assets/app/ocean-background.png'),
   },
   {
     type: AssetType.IMG,
     name: 'chest-opened',
-    src: 'assets/app/chest-opened.png',
+    src: withViteBaseUrlWrapper('assets/app/chest-opened.png'),
   },
   {
     type: AssetType.IMG,
     name: 'chest-closed',
-    src: 'assets/app/chest-closed.png',
+    src: withViteBaseUrlWrapper('assets/app/chest-closed.png'),
   },
   {
     type: AssetType.AUDIO,
     name: 'soundtrack',
-    src: 'assets/music/soundtrack.mp3',
+    src: withViteBaseUrlWrapper('assets/music/soundtrack.mp3'),
   },
 ];
