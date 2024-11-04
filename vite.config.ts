f17ff108f6c68129cc2ac88d52a8c10aac645796
hkhangus <huukhangtc@gmail.com>
2024-11-04 22:25:22 +0700

    feat: config to read env

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), nodePolyfills(), tsconfigPaths()],
-  base: ((process.env.GITHUB_REPOSITORY ?? '') + '/').match(/(\/.*)/)?.[1],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY),
+    },
+    plugins: [react(), nodePolyfills(), tsconfigPaths()],
+    base: ((process.env.GITHUB_REPOSITORY ?? '') + '/').match(/(\/.*)/)?.[1],
+  };
 });
