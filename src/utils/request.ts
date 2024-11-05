ad25204bb0b6535af38e7f88deb5a97fcde1a1a4
nngthinh <dnngthinh@gmail.com>
2024-11-05 07:41:08 +0700

    fix: optional axios request config

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -39,32 +39,38 @@ const baseRequest = <R, D = any>(
   return axiosInstance.request<R>({ method, url, data, headers, params });
 };
 
-export const getWithoutToken = <R>(url: string, { params }: { params?: any }) =>
-  baseRequest<R, undefined>('get', url, { params });
-export const getWithToken = <R>(url: string, { params }: { params?: any }) =>
-  baseRequest<R, undefined>('get', url, { params, auth: true });
+export const getWithoutToken = <R>(
+  url: string,
+  { params }: { params?: any } = {},
+) => baseRequest<R, undefined>('get', url, { params });
+export const getWithToken = <R>(
+  url: string,
+  { params }: { params?: any } = {},
+) => baseRequest<R, undefined>('get', url, { params, auth: true });
 
 export const postWithoutToken = <R, D>(
   url: string,
-  { params, data }: { params?: any; data?: any },
+  { params, data }: { params?: any; data?: any } = {},
 ) => baseRequest<R, D>('post', url, { params, data });
 export const postWithToken = <R, D>(
   url: string,
-  { params, data }: { params?: any; data?: any },
+  { params, data }: { params?: any; data?: any } = {},
 ) => baseRequest<R, D>('post', url, { params, data, auth: true });
 
 export const putWithoutToken = <R, D>(
   url: string,
-  { params, data }: { params?: any; data?: any },
+  { params, data }: { params?: any; data?: any } = {},
 ) => baseRequest<R, D>('put', url, { params, data });
 export const putWithToken = <R, D>(
   url: string,
-  { params, data }: { params?: any; data?: any },
+  { params, data }: { params?: any; data?: any } = {},
 ) => baseRequest<R, D>('put', url, { params, data, auth: true });
 
 export const deleteWithoutToken = <R>(
   url: string,
-  { params }: { params?: any },
+  { params }: { params?: any } = {},
 ) => baseRequest<R, undefined>('delete', url, { params });
-export const deleteWithToken = <R>(url: string, { params }: { params?: any }) =>
-  baseRequest<R, undefined>('delete', url, { params, auth: true });
+export const deleteWithToken = <R>(
+  url: string,
+  { params }: { params?: any } = {},
+) => baseRequest<R, undefined>('delete', url, { params, auth: true });
