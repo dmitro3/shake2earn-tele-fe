c9655dd323fd75d1b0c5931e4ae58c19e1c51526
hkhangus <huukhangtc@gmail.com>
2024-11-07 23:52:19 +0700

    feat: add rule, support, privacy, term

--- src/components/Home/Explore/Footer/index.tsx
+++ src/components/Home/Explore/Footer/index.tsx
@@ -1,13 +1,31 @@
-import { Box, Card, Flex, Text } from '@radix-ui/themes';
+import { Box, Card, Flex } from '@radix-ui/themes';
+
+import DialogCustom from './DialogCustom';
+import Privacy from './Privacy';
+import Rules from './Rules';
+import Support from './Support';
+import Terms from './Terms';
 
 export default function Footer() {
   return (
     <Box className="mt-10">
       <Flex className="justify-center space-x-4 mt-4">
-        <Card className="text-center text-sm">Rules</Card>
-        <Card className="text-center text-sm">Support</Card>
-        <Card className="text-center text-sm">Terms</Card>
-        <Card className="text-center text-sm">Privacy</Card>
+        <DialogCustom
+          title="Rules"
+          description={<Rules />}
+        />
+        <DialogCustom
+          title="Support"
+          description={<Support />}
+        />
+        <DialogCustom
+          title="Privacy"
+          description={<Privacy />}
+        />
+        <DialogCustom
+          title="Terms"
+          description={<Terms />}
+        />
       </Flex>
     </Box>
   );
