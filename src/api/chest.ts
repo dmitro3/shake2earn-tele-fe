f8a6b5265d3d690392b2a3224e95f23879144809
nngthinh <dnngthinh@gmail.com>
2024-11-08 23:18:39 +0700

    feat: update point and turn in app context

--- src/api/chest.ts
+++ src/api/chest.ts
@@ -1,17 +1,26 @@
+import { User } from 'types/user';
 import {
   getWithToken,
   postWithToken,
   withAxiosRequestWrapper,
 } from 'utils/request';
 
-export const getShakeCount = () =>
-  withAxiosRequestWrapper(getWithToken<{ shakeCount: number }>('/shake'));
+export const getShaketurn = () =>
+  withAxiosRequestWrapper(
+    getWithToken<{ shakeCount: number; user: User }>('/shake'),
+  );
 
-export const updateShakeCount = () =>
-  withAxiosRequestWrapper(postWithToken('/shake'));
+export const updateShakeTurn = (turnCount: number) =>
+  withAxiosRequestWrapper(
+    postWithToken<{ user: User }>('/shake', { data: { turnCount } }),
+  );
 
 export const getPoint = () =>
-  withAxiosRequestWrapper(getWithToken<{ point: number }>('/point'));
+  withAxiosRequestWrapper(
+    getWithToken<{ point: number; user: User }>('/point'),
+  );
 
-export const updatePoint = () =>
-  withAxiosRequestWrapper(postWithToken('/point'));
+export const updatePoint = (pointCount: number) =>
+  withAxiosRequestWrapper(
+    postWithToken<{ user: User }>('/point', { data: { pointCount } }),
+  );
