ea94af1d9b882676dc6fb7b1628d2114e0e90eb5
hkhangus <huukhangtc@gmail.com>
2024-11-08 17:07:51 +0700

    chore: css copy invite btn

--- src/components/Home/Explore/User/index.tsx
+++ src/components/Home/Explore/User/index.tsx
@@ -1,11 +1,10 @@
-import * as Toast from '@radix-ui/react-toast';
 import { Box, Button, Card, Heading } from '@radix-ui/themes';
 import WebApp from '@twa-dev/sdk';
 import { useEffect, useState } from 'react';
 
 export default function User() {
   const [user, setUser] = useState<any>();
-  const [open, setOpen] = useState(false);
+  const [copyText, setCopyText] = useState('Copy Invite');
 
   useEffect(() => {
     if (WebApp.initDataUnsafe) {
@@ -14,34 +13,24 @@ export default function User() {
   }, []);
 
   const copyInviteLink = () => {
-    console.log('Invite link copied');
     navigator.clipboard.writeText(
       `t.me/PirateTreasureBot/join?startapp=${user?.id || '123456'}`,
     );
-    setOpen(true);
+    setCopyText('Copied!');
   };
 
   return (
-    <Toast.Provider swipeDirection="right">
-      <Card className=" flex space-x-4">
-        <Box className="flex flex-col">
-          <Heading
-            as="h2"
-            size="3"
-          >
-            {user?.username || 'User Name'}
-          </Heading>
-          <span className=" text-1">ID: {user?.id || '123456'}</span>
-        </Box>
-        <Button onClick={copyInviteLink}>Copy Invite</Button>
-      </Card>
-      <Toast.Root
-        open={open}
-        onOpenChange={setOpen}
-      >
-        <Toast.Title>Invite Link Copied</Toast.Title>
-      </Toast.Root>
-      <Toast.Viewport />
-    </Toast.Provider>
+    <Card className=" flex space-x-4">
+      <Box className="flex flex-col">
+        <Heading
+          as="h2"
+          size="3"
+        >
+          {user?.username || 'User Name'}
+        </Heading>
+        <span className=" text-1">ID: {user?.id || '123456'}</span>
+      </Box>
+      <Button onClick={copyInviteLink}>{copyText}</Button>
+    </Card>
   );
 }
